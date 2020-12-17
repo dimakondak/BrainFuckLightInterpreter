831f2b6d6fdcce28ef999f9db54fef77aa083381
Dmitriy Kondak <dmitriykondak@gmail.com>
2020-12-18 00:54:20 +0200

    replaced to static fill of map

--- src/main/java/com/kondak/commands/CommandFactory.java
+++ src/main/java/com/kondak/commands/CommandFactory.java
@@ -5,16 +5,14 @@ import java.util.Map;
 
 public class CommandFactory {
     public Map<Character, Command> getCommands() {
-        Map<Character, Command> commandMap = new HashMap<>();
-        commandMap.put('+', new IncrementCommand());
-        commandMap.put('-', new DecrementCommand());
-        commandMap.put('>', new RightShiftCommand());
-        commandMap.put('<', new LeftShiftCommand());
-        commandMap.put('.', new OutputCommand());
-
-        commandMap.put(']', new BracketCommand(false));
-        commandMap.put('[', new BracketCommand(true));
-
-        return commandMap;
+        return new HashMap<Character, Command>() {{
+            put('+', new IncrementCommand());
+            put('-', new DecrementCommand());
+            put('>', new RightShiftCommand());
+            put('<', new LeftShiftCommand());
+            put('.', new OutputCommand());
+            put(']', new BracketCommand(false));
+            put('[', new BracketCommand(true));
+        }};
     }
 }
