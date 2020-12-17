9e1c84f5dc688447fdbfe06ca718d58097757fc8
Dmitriy Kondak <dmitriykondak@gmail.com>
2020-12-18 00:06:44 +0200

    made variable "executor" non-static in RunExecutor

--- src/main/java/com/kondak/RunExecutor.java
+++ src/main/java/com/kondak/RunExecutor.java
@@ -4,7 +4,6 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 public class RunExecutor {
-    private static final Executor EXECUTOR = new Executor();
     private static final Logger log = LogManager.getLogger();
 
     public static void main(String[] args) {
@@ -12,7 +11,8 @@ public class RunExecutor {
         final String CODE = "++++++++[>++++[>++>+++>+++>+<<<<-]>+>+>->>+[<]<-]>>.>---.+++++++..+++.>>.<-.<.+++.------.--------.>>+.>++.";
 
         log.info("Starting program");
-        EXECUTOR.execute(CODE);
+        Executor executor = new Executor();
+        executor.execute(CODE);
         log.info("Program completed successfully");
     }
 }
